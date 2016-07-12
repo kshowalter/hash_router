acadcef0e348934cfa2c220198243c10de863c50
Keith Showalter <kshowalter@fsec.ucf.edu>
2016-07-12 17:01:17 -0400

    First version

---
+++ index.js
@@ -0,0 +1,26 @@
+module.exports = function(cb){
+
+  function router(cb) {
+    var request = location.hash.slice(2) || '';
+
+    cb(request);
+
+  }
+
+  // Listen on hash change:
+  window.addEventListener('hashchange', function(){
+    router(cb);
+  });
+  // Listen on page load:
+  window.addEventListener('load', function(){
+    router(cb);
+  });
+
+  return function(location){
+    if( location instanceof String ){
+      window.location.hash = '#/' + location;
+    } else {
+      console.warn('location not a string');
+    }
+  };
+};
